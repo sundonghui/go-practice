55b692369fca324972d04435064d5bc4d1fa3bf4
sundonghui <sundonghui@powerlaw.ai>
2025-02-14 23:59:13 +0800

    [modified] channel test

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -1,6 +1,7 @@
 package channel
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -61,4 +62,38 @@ func Test_buffer_lock_5(t *testing.T) {
 	t.Log("end1")
 	bc <- 1
 	t.Log("end2")
+}
+
+func Test_wait_group(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	t.Log("end0") // panic: sync: negative WaitGroup counter [recovered]
+	wg.Done()
+	t.Log("end1") // panic: sync: negative WaitGroup counter
+	wg.Done()
+	t.Log("end2")
+	wg.Done()
+	t.Log("end3")
+	wg.Wait()
+	t.Log("end4")
+}
+
+func Test_cond_1(t *testing.T) {
+	var mu sync.Mutex
+	var cond = sync.NewCond(&mu)
+	mu.Lock()
+	cond.Wait() // panic: test timed out after 30s
+	t.Log("end0")
+	cond.Wait()
+	t.Log("end1")
+	cond.Wait()
+	t.Log("end2")
+	mu.Unlock()
+
+	go func() {
+		mu.Lock()
+		cond.Signal() // signal waiting goroutine
+		mu.Unlock()
+	}()
+
 }
